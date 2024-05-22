36e21f23ed5efebb13d76ed6e23438cb22adb159
rresendi <153397132+rresendi@users.noreply.github.com>
2024-05-22 14:10:01 -0400

    Update MC_electron_efficiency.py

--- MC_electron_efficiency.py
+++ MC_electron_efficiency.py
@@ -1,7 +1,7 @@
 import uproot
 import os
 import argparse
-
+import awkward as ak
 import numpy as np
 import ROOT
 
@@ -20,7 +20,7 @@ args = vars(parser.parse_args())
 sample_name = args["input"]
 
 output_file = "MC_electron_efficiencies.root"
-input_file= "/eos/user/j/jreicher/SUEP/WH_private_signals/merged/"+sample_name + ".root"
+input_file = "/eos/user/j/jreicher/SUEP/WH_private_signals/merged/" + sample_name + ".root"
 
 # suep decay type
 
@@ -92,18 +92,6 @@ elif "T32.00" in sample_name:
 else:
     temp = "6.00"
 
-# Defining a good electron
-electrons = ak.zip({
-        "pt": events.Electron.pt,
-        "eta": events.Electron.eta,
-        "phi": events.Electron.phi,
-        "mass": events.Electron.mass,
-        "charge": events.Electron.pdgId/(-11),
-        "pdgId": events.Electron.pdgId,
-        "isTight": events.Electron.mvaFall17V2Iso_WP80,
-        "isTightIso": events.Electron.mvaFall17V2Iso_WP80
-        }, with_name="Momentum4D")
-
 # Gets relevant events from file
 def Events(f):
     evs = f['Events'].arrays(['HLT_Ele32_WPTight_Gsf',
@@ -116,32 +104,56 @@ def Events(f):
                 'Electron_eta',
                 'Electron_dxy',
                 'Electron_dz',
+                'Electron_phi',
+                'Electron_mass',
+                'Electron_pdgId',
+                'TrigObj_pt',
+                'TrigObj_eta',
+                'TrigObj_phi',
+                'TrigObj_id',
+                'TrigObj_filterBits'
                 ])
     return evs
-    
+
+with uproot.open(input_file) as f:
+    evs = Events(f)
+
+# Defining a good electron
+electrons = ak.zip({
+        "pt": evs["Electron_pt"],
+        "eta": evs["Electron_eta"],
+        "phi": evs["Electron_phi"],
+        "mass": evs["Electron_mass"],
+        "charge": evs["Electron_pdgId"]/(-11),
+        "pdgId": evs["Electron_pdgId"],
+        "isTight": evs["Electron_mvaFall17V2Iso_WP80"],
+        "isTightIso": evs["Electron_mvaFall17V2Iso_WP80"]
+        }, with_name = "Momentum4D")   
+
 # Gets matched online/offline electrons from file                                                                                                                              
 def isHLTMatched(self, events):
     trigObj = ak.zip({
-            "pt": events.TrigObj.pt,
-            "eta": events.TrigObj.eta,
-            "phi": events.TrigObj.phi,
+            "pt": events["TrigObj_pt"],
+            "eta": events["TrigObj_eta"],
+            "phi": events['TrigObj_phi'],
             "mass": 0.,
-            "id": events.TrigObj.id,
-            "filterBits": events.TrigObj.filterBits
-        }, with_name="Momentum4D")
-        trigObjSingleEl = trigObj[((abs(trigObj.id) == 11) &
-                               ((trigObj.filterBits & 2) |
-                                (trigObj.filterBits & 2048) |
-                                (trigObj.filterBits & 8192)))]
+            "id": events['TrigObj_id'],
+            "filterBits": events['TrigObj_filterBits']
+        }, with_name = "Momentum4D")
+    
+    trigObjSingleEl = trigObj[((abs(trigObj.id) == 11) &
+                               ((events['trigObj_filterBits'] & 2) |
+                                (events['trigObj_filterBits'] & 2048) |
+                                (events['trigObj_filterBits'] & 8192)))]
 
-    toMatch1El, trigObjSingleEl = ak.unzip(ak.cartesian(electrons, trigObjSingleEl], axis=1, nested = True))
+    toMatch1El, trigObjSingleEl = ak.unzip(ak.cartesian([electrons], trigObjSingleEl, axis=1, nested = True))
     alldr2                      = toMatch1El.deltaR2(trigObjSingleEl)
     match1El                    = (ak.sum(ak.where(ak.min(alldr2, axis=2) < 0.1, True, False), axis = 1) >= 1)
 
     return match1El
 
 # Defines binning and histograms
-ele_bin_edges=array('d',[0,2,4,6,8,10,12,
+ele_bin_edges = array('d',[0,2,4,6,8,10,12,
                          14,16,18,20,22,
                          24,26,28,30,32,
                          34,36,38,40,50,
@@ -207,8 +219,6 @@ def ele_hists(events, etas, hists):
 
     return 0
 
-with uproot.open(input_file) as f:
-    evs = Events(f)
     eta_split = [[0, 2.5],[0.0, 0.7], [0.7,1.444], [1.566,2], [2, 2.5]]
     eta_hists = [[ele_totalhist,ele_filthist],[eta1_ele_totalhist,eta1_ele_filthist],
                  [eta2_ele_totalhist,eta2_ele_filthist],[eta3_ele_totalhist,eta3_ele_filthist],
@@ -216,7 +226,9 @@ with uproot.open(input_file) as f:
     
     for (etas,hists) in zip(eta_split, eta_hists):
         ele_hists(evs,etas,hists)
+
 # Fills efficiency
+
 eta1_effs = ROOT.TEfficiency(eta1_ele_filthist,eta1_ele_totalhist)
 eta2_effs = ROOT.TEfficiency(eta2_ele_filthist,eta2_ele_totalhist)
 eta3_effs = ROOT.TEfficiency(eta3_ele_filthist,eta3_ele_totalhist)
@@ -224,6 +236,7 @@ eta4_effs = ROOT.TEfficiency(eta4_ele_filthist,eta4_ele_totalhist)
 c1 = ROOT.TCanvas ("canvas","",800,600)
 
 # Get overall Efficiency:
+
 ele_eff = ele_filthist.Clone()
 
 ele_eff.SetName(sample_name)
@@ -239,13 +252,13 @@ ele_eff.Divide(ele_totalhist)
 
 eta1_effs.SetTitle("Electron Trigger Efficiency in bins of pT;Electron pT [GeV];Efficiency")
 legend = ROOT.TLegend(0.5,0.1,0.9,0.4)
-legend.AddEntry(eta1_effs,"|#eta|<0.7","l")
-legend.AddEntry(eta2_effs,"0.7<|#eta|<1.444","l")
-legend.AddEntry(eta3_effs,"1.566<|#eta|<2","l")
-legend.AddEntry(eta4_effs,"2<|#eta|<2.5","l")
-legend.AddEntry(ROOT.nullptr, "T = "+temp+"GeV"+year,"")
-legend.AddEntry(ROOT.nullptr,"SUEP decay type: "+decay_type,"")
-legend.AddEntry(ROOT.nullptr,"Dark meson mass = "+ md,"")
+legend.AddEntry(eta1_effs, "|#eta|<0.7", "l")
+legend.AddEntry(eta2_effs, "0.7<|#eta|<1.444", "l")
+legend.AddEntry(eta3_effs, "1.566<|#eta|<2", "l")
+legend.AddEntry(eta4_effs, "2<|#eta|<2.5", "l")
+legend.AddEntry(ROOT.nullptr, "T = " + temp + "GeV" + year, "")
+legend.AddEntry(ROOT.nullptr, "SUEP decay type: " + decay_type, "")
+legend.AddEntry(ROOT.nullptr, "Dark meson mass = " + md, "")
 legend.SetTextColor(ROOT.kBlack)
 legend.SetTextFont(42)
 legend.SetTextSize(0.03)
@@ -261,8 +274,10 @@ eta4_effs.SetLineColor(ROOT.kGreen)
 eta4_effs.Draw("same")
 legend.Draw("same")
 c1.Update()
+
 # Saves to pdf
-c1.SaveAs(folder+sample_name+"_Efficiency.pdf")
+
+c1.SaveAs(folder + sample_name + "_Efficiency.pdf")
 
 # Saves overall efficiency
 
@@ -288,4 +303,4 @@ except (OSError, IOError) as e:
 
 
 
-print("sample "+sample_name+" complete")
+print("sample " + sample_name + " complete")
