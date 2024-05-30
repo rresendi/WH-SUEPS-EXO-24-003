d40795a950a7b36ff500dda44bd1cbdbee66e91a
rresendi <153397132+rresendi@users.noreply.github.com>
2024-05-30 14:16:25 -0400

    Update deltaRhists.py

--- deltaRhists.py
+++ deltaRhists.py
@@ -6,21 +6,21 @@ import numpy as np
 import ROOT
 from array import array
 
-# Sets batch mode so no popup window
+# Sets batch mode so no popup window                                                                                                                                                                       
 ROOT.gROOT.SetBatch(True)
 
-# Initialize parser
+# Initialize parser                                                                                                                                                                                        
 parser = argparse.ArgumentParser()
 parser.add_argument("--input", help="Name of input file", type=str)
 args = vars(parser.parse_args())
 
-# Name of sample
+# Name of sample                                                                                                                                                                                           
 sample_name = args["input"]
 
 output_file = "MC_electron_efficiencies.root"
 input_file = "/eos/user/j/jreicher/SUEP/WH_private_signals/merged/" + sample_name + ".root"
 
-# suep decay type
+# suep decay type                                                                                                                                                                                          
 if "generic" in sample_name:
     decay_type = "generic"
 elif "hadronic" in sample_name:
@@ -28,20 +28,18 @@ elif "hadronic" in sample_name:
 else:
     decay_type = "leptonic"
 
-# conditions for what year
+# conditions for what year                                                                                                                                                                                 
 if "UL18" in sample_name:
-    year = "2018 conditions"
-    folder = "ele_eff_outputs_2018/"
-elif "UL17" in sample_name:
-    year = "2017 conditions"
+    year = "2018 conditions"    
     folder = "ele_eff_outputs_2017/"
 elif "UL16APV" in sample_name:
+    year = "2016 APV conditions"
     folder = "ele_eff_outputs_2016APV/"
 else:
     year = "2016 conditions"
     folder = "ele_eff_outputs_2016/"
 
-# dark meson (phi) mass
+# dark meson (phi) mass                                                                                                                                                                                    
 if "MD2.00" in sample_name:
     md = "2.00 [GeV]"
 elif "MD4.00" in sample_name:
@@ -55,7 +53,7 @@ elif "MD1.00" in sample_name:
 else:
     md = "1.40 [GeV]"
 
-# temperature
+# temperature                                                                                                                                                                                              
 if "T0.25" in sample_name:
     temp = "0.25"
 if "T0.35" in sample_name:
@@ -85,7 +83,7 @@ elif "T32.00" in sample_name:
 else:
     temp = "6.00"
 
-# Gets relevant events from file
+# Gets relevant events from file                                                                                                                                                                           
 def Events(f):
     evs = f['Events'].arrays(['HLT_Ele32_WPTight_Gsf',
                               'HLT_Ele115_CaloIdVT_GsfTrkIdT',
@@ -111,7 +109,8 @@ def Events(f):
 with uproot.open(input_file) as f:
     evs = Events(f)
 
-# Defining a good electron                                                                                                                                                                                 
+# Defining a good electron                                                                                                                                                                                \
+                                                                                                                                                                                                           
 
 electrons = ak.zip({
         "pt": evs["Electron_pt"],
@@ -136,14 +135,14 @@ cutElectrons = (
 
 goodElectrons = electrons[cutElectrons]
 
-# Computes deltaR2
+# Computes deltaR2                                                                                                                                                                                         
 def deltaR2(eta1, phi1, eta2, phi2):
     deta = eta1 - eta2
     dphi = np.arccos(np.cos(phi1 - phi2))
 
     return deta ** 2 + dphi ** 2
 
-# Gets matched online/offline electrons from file
+# Gets matched online/offline electrons from file                                                                                                                                                          
 def isHLTMatched(events, electrons):
     trigObj = ak.zip({
         "pt": events["TrigObj_pt"],
@@ -152,7 +151,7 @@ def isHLTMatched(events, electrons):
         "mass": 0.,
         "id": events['TrigObj_id'],
         "filterBits": events['TrigObj_filterBits']
-    }, with_name="Momentum4D")
+        }, with_name="Momentum4D")
 
     trigObjSingleEl = trigObj[((abs(trigObj.id) == 11) &
                                ((events['TrigObj_filterBits'] & 2) |
@@ -165,7 +164,7 @@ def isHLTMatched(events, electrons):
 
     return match1El
 
-# Defines binning and histograms
+# Defines binning and histograms                                                                                                                                                                           
 ele_bin_edges = array('d', [0, 2, 4, 6, 8, 10, 12,
                             14, 16, 18, 20, 22,
                             24, 26, 28, 30, 32,
@@ -173,62 +172,82 @@ ele_bin_edges = array('d', [0, 2, 4, 6, 8, 10, 12,
                             60, 70, 80, 90, 100,
                             120, 140, 160, 180, 200])
 
-# Create a 2D histogram for deltaR vs. pT for each eta bin
+# Create a 2D histogram for deltaR vs. pT for each eta bin                                                                                                                                                 
 deltaR_vs_pt_eta1 = ROOT.TH2F("deltaR_vs_pt_eta1", "DeltaR vs pT (|eta| < 1)", 30, 0, 200, 100, 0, 0.5)
 deltaR_vs_pt_eta2 = ROOT.TH2F("deltaR_vs_pt_eta2", "DeltaR vs pT (1 <= |eta| < 2)", 30, 0, 200, 100, 0, 0.5)
 deltaR_vs_pt_eta3 = ROOT.TH2F("deltaR_vs_pt_eta3", "DeltaR vs pT (2 <= |eta| < 2.5)", 30, 0, 200, 100, 0, 0.5)
 
-# Function for filling the deltaR histograms and 2D histograms
+# Function for filling the deltaR histograms and 2D histograms                                                                                                                                             
 def ele_hists(events, etas, deltaR_vs_pt_hist):
     eta_min, eta_max = etas
 
-    # Electron selection
+    # Electron selection                                                                                                                                                                                   
     ele_quality_check = isHLTMatched(events, goodElectrons)
 
-    # Cut on eta
+    # Trigger selection                                                                                                                                                                                    
+
+    if "UL17" in sample_name or "UL18" in sample_name:
+        triggerSingleElectron = (
+            events["HLT_Ele32_WPTight_Gsf"] |
+            events["HLT_Ele115_CaloIdVT_GsfTrkIdT"] |
+            events["HLT_Photon200"]
+        )
+    else:
+        triggerSingleElectron = (
+            events["HLT_Ele32_WPTight_Gsf"] |
+            events["HLT_Ele115_CaloIdVT_GsfTrkIdT"] |
+            events["HLT_Photon175"]
+        )
+    
+    # Cut on eta                                                                                                                                                                                           
     eta_split = (
         (np.abs(events["Electron_eta"]) >= eta_min) &
         (np.abs(events["Electron_eta"]) < eta_max)
     )
+    
 
-    # Compute deltaR and fill 2D histograms
+    # Compute deltaR and fill 2D histograms                                                                                                                                                                
     trigObj = ak.zip({
         "eta": events["TrigObj_eta"],
         "phi": events['TrigObj_phi']
-    }, with_name="Momentum4D")
+    }, with_name = "Momentum4D")
     trigObjSingleEl = trigObj[((abs(events['TrigObj_id']) == 11) &
                                ((events['TrigObj_filterBits'] & 2) |
                                 (events['TrigObj_filterBits'] & 2048) |
                                 (events['TrigObj_filterBits'] & 8192)))]
-    toMatch1El, trigObjSingleEl = ak.unzip(ak.cartesian([goodElectrons, trigObjSingleEl], axis=1, nested=True))
+    toMatch1El, trigObjSingleEl = ak.unzip(ak.cartesian([goodElectrons, trigObjSingleEl], axis = 1, nested = True))
     alldr2 = deltaR2(toMatch1El.eta, toMatch1El.phi, trigObjSingleEl.eta, trigObjSingleEl.phi)
     deltaR_values = ak.min(alldr2, axis=2)
-    electron_pts = events["Electron_pt"]
+    
+    
+    ele = events["Electron_pt"]
+    evs = ele[ele_quality_check & eta_split]
+    tr_evs = evs[triggerSingleElectron]
 
-    for deltaR, pt in zip(deltaR_values[ele_quality_check & eta_split], electron_pts[ele_quality_check & eta_split]):
+    for deltaR, pt in zip(deltaR_values[both], ele[both]):
         for value, pt_val in zip(deltaR, pt):
             deltaR_vs_pt_hist.Fill(pt_val, value)
 
-# Fill 2D histograms
+# Fill 2D histograms                                                                                                                                                                                       
 eta_bins = [(0, 1, deltaR_vs_pt_eta1), (1, 2, deltaR_vs_pt_eta2), (2, 2.5, deltaR_vs_pt_eta3)]
 for (eta_min, eta_max, deltaR_vs_pt_hist) in eta_bins:
     ele_hists(evs, (eta_min, eta_max), deltaR_vs_pt_hist)
 
-# Create canvas for plotting
+# Create canvas for plotting                                                                                                                                                                               
 c1 = ROOT.TCanvas("canvas", "", 800, 600)
-c1.SetRightMargin(0.15)  # Adjust right margin to accommodate legend
+c1.SetRightMargin(0.15)  # Adjust right margin to accommodate legend                                                                                                                                       
 
-# Draw the first 2D histogram
+# Draw the first 2D histogram                                                                                                                                                                              
 deltaR_vs_pt_eta1.Draw("COLZ")
 deltaR_vs_pt_eta1.GetXaxis().SetTitle("pT [GeV]")
 deltaR_vs_pt_eta1.GetYaxis().SetTitle("DeltaR")
 c1.Update()
 
-# Draw subsequent histograms on top of the first one
+# Draw subsequent histograms on top of the first one                                                                                                                                                       
 deltaR_vs_pt_eta2.Draw("COLZ SAME")
 deltaR_vs_pt_eta3.Draw("COLZ SAME")
 
-# Add a legend
+# Add a legend                                                                                                                                                                                             
 legend = ROOT.TLegend(0.7, 0.15, 0.85, 0.4)
 legend.AddEntry(deltaR_vs_pt_eta1, "|eta| < 1", "l")
 legend.AddEntry(deltaR_vs_pt_eta2, "1 <= |eta| < 2", "l")
@@ -236,5 +255,5 @@ legend.AddEntry(deltaR_vs_pt_eta3, "2 <= |eta| < 2.5", "l")
 legend.SetTextSize(0.03)
 legend.Draw()
 
-# Save the plot
+# Save the plot                                                                                                                                                                                            
 c1.SaveAs(folder + sample_name + "_DeltaR_vs_pT_by_Eta.pdf")
