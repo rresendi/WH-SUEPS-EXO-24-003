02268fac682d13a9102732a5e28b566a2c12134e
rresendi <153397132+rresendi@users.noreply.github.com>
2024-06-26 12:10:29 -0400

    Update simplesignal.py

--- simplesignal.py
+++ simplesignal.py
@@ -1,26 +1,22 @@
 import uproot
 import os
 import argparse
-
+import awkward as ak
 import numpy as np
 import ROOT
-
 from array import array
 
-# Sets batch mode so no popup window                                                                                                                                                                                  
+# Sets batch mode so no popup window                                                                                                                                                                       
 ROOT.gROOT.SetBatch(True)
 
-import argparse, types
-
-
-# Initialize parser                                                                                                                                                                                                   
+# Initialize parser                                                                                                                                                                                        
 parser = argparse.ArgumentParser()
 
 parser.add_argument("--input", help = "Name of input file", type = str)
 args = vars(parser.parse_args())
 
-# Name of sample                                                                                                                                                                                                      
-sample_name= args["input"]
+# Name of sample                                                                                                                                                                                           
+sample_name = args["input"]
 
 output_file = "MC_electron_efficiencies.root"
 input_file = "/eos/user/j/jreicher/SUEP/WH_private_signals/merged/" + sample_name + ".root"
@@ -93,25 +89,28 @@ elif "T32.00" in sample_name:
     temp = "32.00"
 else:
     temp = "6.00"
-    
-# Gets relevant variables from file                                                                                                                                                                                   
+
+# Gets relevant events from file                                                                                                                                                                           
+
 def Events(f):
     evs = f['Events'].arrays(['HLT_Ele32_WPTight_Gsf',
-                'HLT_Ele115_CaloIdVT_GsfTrkId',
-                'Electron_mvaFall17V2Iso_WP80',
+                'HLT_Ele115_CaloIdVT_GsfTrkIdT',
                 'HLT_Photon175',
                 'HLT_Photon200',
+                'Electron_cutBased',
                 'Electron_pt',
+                'Electron_mvaFall17V2Iso_WP80',
                 'Electron_eta',
-                'Electron_dz',
                 'Electron_dxy',
-                'Electron_cutBased'])
+                'Electron_dz',
+                ])
     return evs
 
 with uproot.open(input_file) as f:
     evs = Events(f)
 
-# Defines binning and histograms                                                                                                                                                                                      
+# Defines binning and histograms                                                                                                                                                                           
+
 ele_bin_edges = array('d',[0,2,4,6,8,10,12,
                          14,16,18,20,22,
                          24,26,28,30,32,
@@ -135,20 +134,29 @@ eta3_ele_filthist = ROOT.TH1D("filt_events","Filtered Events",len(ele_bin_edges)
 eta4_ele_totalhist = ROOT.TH1D("total_events","Total Events",len(ele_bin_edges)-1,ele_bin_edges)
 eta4_ele_filthist = ROOT.TH1D("filt_events","Filtered Events",len(ele_bin_edges)-1,ele_bin_edges)
 
-# Function for filling the histograms                                                                                                                                                                                 
+# Function for filling the histograms                                                                                                                                                                      
 
-def ele_hists(events,etas,hists):
+def ele_hists(events, etas, hists):
     ele_totalhist = hists[0]
     ele_filthist = hists[1]
     eta_min = etas[0]
     eta_max = etas[1]
-    # trigger                                                                                                                                                                                                         
-    triggerSingleElectron = (
-            events["HLT_Ele32_WPTight_Gsf"]
-            | events["HLT_Ele115_CaloIdVT_GsfTrkIdT"]
-            | events["HLT_Photon175"]
-            | events["HLT_Photon200"]
+    
+    # Trigger selection                                                                                                                                                                                    
+
+    if "UL17" in sample_name or "UL18" in sample_name:
+        triggerSingleElectron = (
+            events["HLT_Ele32_WPTight_Gsf"] |
+            events["HLT_Ele115_CaloIdVT_GsfTrkIdT"] |
+            events["HLT_Photon200"]
         )
+    else:
+        triggerSingleElectron = (
+            events["HLT_Ele32_WPTight_Gsf"] |
+            events["HLT_Ele115_CaloIdVT_GsfTrkIdT"] |
+            events["HLT_Photon175"]
+        )
+    
     # quality requirements for electrons                                                                                                                                                                              
     ele_quality_check = (
             (events["Electron_cutBased"] >= 2)
@@ -159,17 +167,21 @@ def ele_hists(events,etas,hists):
             & (abs(events["Electron_eta"]) < 2.5)
             )
     
-    # cut on eta                                                                                                                                                                                                      
+    # Cut on eta                                                                                                                                                                                           
+
     eta_split = (
         (np.abs(events["Electron_eta"]) >= eta_min)
-        & (np.abs(events["Electron_eta"]) < eta_max )
+        & (np.abs(events["Electron_eta"]) < eta_max)
     )
-    # Select based on trigger                      
+
+    # Select based on trigger                                                                                                                                                                              
+
     ele = events["Electron_pt"]
     evs = ele[ele_quality_check & eta_split]
     tr_evs = evs[triggerSingleElectron]
 
-    #Fill histograms                                                                                                                                                                                                  
+    # Fill histograms                                                                                                                                                                                      
+
     for ev in evs:
         for entry in ev:
             ele_totalhist.Fill(entry)
@@ -179,31 +191,35 @@ def ele_hists(events,etas,hists):
 
     return 0
 
-with uproot.open(input_file) as f:
-    evs = Events(f)
-    eta_split = [[0, 1],[1, 2],[2, 3]]
-    eta_hists = [[eta1_ele_totalhist,eta1_ele_filthist],
+eta_split = [[0, 1.0], [1.0, 2.0], [2.0, 3.0]]
+eta_hists = [[eta1_ele_totalhist,eta1_ele_filthist],
              [eta2_ele_totalhist,eta2_ele_filthist],
              [eta3_ele_totalhist,eta3_ele_filthist]]
-    for (etas,hists) in zip(eta_split,eta_hists):
-        ele_hists(evs,etas,hists)
 
-# Fills efficiency                                                                                                                                                                                                    
+for (etas,hists) in zip(eta_split, eta_hists):
+    ele_hists(evs, etas, hists)
+
+# Fills efficiency                                                                                                                                                                                         
+
 eta1_effs = ROOT.TEfficiency(eta1_ele_filthist,eta1_ele_totalhist)
 eta2_effs = ROOT.TEfficiency(eta2_ele_filthist,eta2_ele_totalhist)
 eta3_effs = ROOT.TEfficiency(eta3_ele_filthist,eta3_ele_totalhist)
 c1 = ROOT.TCanvas ("canvas","",800,600)
 
 # Get overall Efficiency:                                                                                                                                                                                  
+
 ele_eff = ele_filthist.Clone()
 
-# Creates Efficiency Plot w legend                                                                                                                                                                                    
+# Creates Efficiency Plot w legend                                                                                                                                                                         
 
 eta1_effs.SetTitle("Electron Trigger Efficiency in bins of pT;Electron pT [GeV];Efficiency")
 legend = ROOT.TLegend(0.5,0.1,0.9,0.4)
-legend.AddEntry(eta1_effs,"|#eta|<1","l")
-legend.AddEntry(eta2_effs,"1<|#eta|<2","l")
-legend.AddEntry(eta3_effs,"2<|#eta|<3","l")
+legend.AddEntry(eta1_effs, "|#eta|<1.0", "l")
+legend.AddEntry(eta2_effs, "1.0<|#eta|<2.0", "l")
+legend.AddEntry(eta3_effs, "2.0<|#eta|<3.0", "l")
+legend.AddEntry(ROOT.nullptr, "T = " + temp + "GeV, " + year,"")
+legend.AddEntry(ROOT.nullptr, "SUEP decay type: " + decay_type,"")
+legend.AddEntry(ROOT.nullptr, "Dark meson mass = " + md,"")
 legend.SetTextColor(ROOT.kBlack)
 legend.SetTextFont(42)
 legend.SetTextSize(0.03)
@@ -240,11 +256,9 @@ eta2_effs.SetLineColor(ROOT.kRed)
 eta3_effs.Draw("P same")
 eta3_effs.SetLineColor(ROOT.kBlue)
 
-# Saves to pdf                                                                                                                                                                           \
-                                                                                                                                                                                          
+# Saves to pdf
 
 c1.SaveAs(sample_name + "_Efficiency.pdf")
-
 # Saves overall efficiency                                                                                                                                                                                 
 
 root_file = ROOT.TFile(output_file, "UPDATE")
