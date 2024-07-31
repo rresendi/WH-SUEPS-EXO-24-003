647d27bcc482d9e97eced5de1350b5ef46bbe84a
rresendi <153397132+rresendi@users.noreply.github.com>
2024-07-31 10:18:33 -0400

    Update for_data.py

--- for_data.py
+++ for_data.py
@@ -1,29 +1,29 @@
-def reference_cuts(events, era, scouting, met):
+def reference_cuts(events, self, met):
     # Apply lumi mask
-    if (era == "2016" or era == "2016apv") and scouting != 1:
+    if (self.era == "2016" or self.era == "2016apv") and self.scouting != 1:
         LumiJSON = lumi_tools.LumiMask(
             "data/GoldenJSON/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON.txt"
         )
-    elif era == "2016" and scouting == 1:
+    elif self.era == "2016" and self.scouting == 1:
         LumiJSON = lumi_tools.LumiMask(
             "data/GoldenJSON/Cert_271036-284044_13TeV_Legacy2016_Collisions16_JSON_scout.txt"
         )
-    elif era == "2016apv" and scouting == 1:
+    elif self.era == "2016apv" and self.scouting == 1:
         LumiJSON = lumi_tools.LumiMask(
             "data/GoldenJSON/Cert_271036-284044_13TeV_Legacy2016_Collisions16APV_JSON_scout.txt"
         )
-    elif era == "2017":
+    elif self.era == "2017":
         LumiJSON = lumi_tools.LumiMask(
             "data/GoldenJSON/Cert_294927-306462_13TeV_UL2017_Collisions17_GoldenJSON.txt"
         )
-    elif era == "2018":
+    elif self.era == "2018":
         LumiJSON = lumi_tools.LumiMask(
             "data/GoldenJSON/Cert_314472-325175_13TeV_Legacy2018_Collisions18_JSON.txt"
         )
     else:
         raise ValueError("No era is defined. Please specify the year")
 
-    if scouting == 1:
+    if self.scouting == 1:
         events = events[LumiJSON(events.run, events.lumSec)]
     else:
         events = events[LumiJSON(events.run, events.luminosityBlock)]
@@ -33,7 +33,7 @@ def reference_cuts(events, era, scouting, met):
     events = events[met_cut]
 
     # Apply quality filters
-    if era == "2018" or era == "2017":
+    if self.era == "2018" or self.era == "2017":
         cutAnyFilter = (
             (events.Flag.goodVertices)
             & (events.Flag.globalSuperTightHalo2016Filter)
@@ -45,7 +45,7 @@ def reference_cuts(events, era, scouting, met):
             & (events.Flag.eeBadScFilter)
             & (events.Flag.ecalBadCalibFilter)
         )
-    elif era == "2016" or era == "2016apv":
+    elif self.era == "2016" or self.era == "2016apv":
         cutAnyFilter = (
             (events.Flag.goodVertices)
             & (events.Flag.globalSuperTightHalo2016Filter)
